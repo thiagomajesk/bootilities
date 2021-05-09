0713a9e871e56b4c05158b56620371e6c374b386
thiagomajesk <thiagomgoulart@gmail.com>
2021-05-09 18:40:21 -0300

    refactor: remove indirect dependencies

--- sync-vendor.sh
+++
@@ -1,3 +0,0 @@
-wget -q --show-progress -N -P scss/vendor/bootstrap https://raw.githubusercontent.com/twbs/bootstrap/main/scss/_functions.scss
-wget -q --show-progress -N -P scss/vendor/bootstrap https://raw.githubusercontent.com/twbs/bootstrap/main/scss/_variables.scss
-wget -q --show-progress -N -P scss/vendor/bootstrap https://raw.githubusercontent.com/twbs/bootstrap/main/scss/_utilities.scss
